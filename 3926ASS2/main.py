5be331fd82079e2555ccfaa1556d96d7878772e8
Spencer Kelly <1spencerkelly@gmail.com>
2023-03-03 00:25:19 -0500

    almost done

--- 3926ASS2/main.py
+++ 3926ASS2/main.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 
 #part 1
-def motion_solver(iSpeed, launchAngle, timeStep, method = 'Midpoint', airRes = True):
+def motion_solver(iSpeed, launchAngle, timeStep, method = 'Midpoint', airRes = True, part3 = False):
     '''Solves equation of motion for a baseball with initial speed and angle using specified method, and removes air resistance if requested. Note: Midpoint method is assumed if no method argument is given (due to highest accuracy).'''
     #List all constants used in the calculation:
     #according to the textbook, we can approximate the drag coefficient as 0.35
@@ -57,15 +57,21 @@ def motion_solver(iSpeed, launchAngle, timeStep, method = 'Midpoint', airRes = T
             nextPos = currPos + currVel*timeStep - 1/2*accel*(timeStep**2)
             nextVel = currVel + accel * timeStep
 
+        #if the function is being used for part 3, return the height value of the ball at the first x value after 400m
+        if part3 and currPos[0] >= ft_to_m(400):
+            return currPos[1]
 
-
-
-
+        #update the current position for the next iteration of the while loop
         currPos = nextPos
         currVel = nextVel
+        #append the current position to the list of positions
         xPos = np.append(xPos, currPos[0])
         yPos = np.append(yPos, currPos[1])
-    return xPos, yPos
+    #return the two position arrays in the case the function is not being used for part 3
+    if not part3:
+        return xPos, yPos
+    #if the function is being used for part 3 and 400m was never reached by the ball before it hit the ground, return a value of -1 as the y pos
+    elif part3: return -1
 
 def mph_to_mps(mph):
     '''converts mph to m/s'''
@@ -76,7 +82,7 @@ def ft_to_m(feet):
     return 0.3048*feet
 
 #function for part 2
-def ABvsHRRats(speed, spd_std, angle, ang_std, length, field_dist):
+def ABvsHRRats(speed, spd_std, angle, ang_std, length, field_dist, part3 = False, fenceHeight = None):
     '''Creates an array of specified length with a gaussian distribution around the specified speed input with standard deviation of specified parameter
     and an array of same length with a gaussian distribution around the launch angle input parameter with standard deviation of specified input'''
     #create an array for a random gaussian distribution of velocity with standard deviation as specified by parameter
@@ -89,11 +95,17 @@ def ABvsHRRats(speed, spd_std, angle, ang_std, length, field_dist):
     HRCount = 0
     #iterate through the two arrays, generating a solution of motion for all initial speeds and their corresponding launch angles
     for iSpeed, iAngle, in zip(speedArray, angleArray):
-        xPos, yPos = motion_solver(iSpeed, iAngle, 0.1)
         ABCount += 1
-        #if the final x position is greater than 400m, add one to the home run counter
-        if xPos[-1] >= ft_to_m(field_dist):
-            HRCount += 1
+        if not part3:
+            xPos, yPos = motion_solver(iSpeed, iAngle, 0.1, part3 = part3)
+            #if the final x position is greater than 400m, add one to the home run counter
+            if xPos[-1] >= ft_to_m(field_dist):
+                HRCount += 1
+        elif part3:
+            yPos = motion_solver(iSpeed, iAngle, 0.1, part3 = part3)
+            if yPos >= fenceHeight:
+                HRCount += 1
+
     #return the ratio
     try:
         return ABCount/HRCount
@@ -101,8 +113,20 @@ def ABvsHRRats(speed, spd_std, angle, ang_std, length, field_dist):
         return str(ABCount) + ' at bats and not a single home run :('
 
 #function for part 3:
-def optimal_fence_height(xArr, yArr)
+def optimal_fence_height():
+    '''This function serves to analyze the AB/HR ratio for different fenceheights'''
+    #create an array of all fenceheights to be tested
+    heightsArray = np.arange(0.5, 15.1, 0.1)
+    #create an empty array to append the AB/HR ratios to
+    HRArray = np.array([])
+    #iterate through each fenceheight
+    for i in heightsArray:
+        #Calculate the ABvsHR value for the given fenceheight by implementing the part3 argument for the ABvsHR function
+        ABvsHR = ABvsHRRats(100, 15, 45, 10, 300, 400, part3=True, fenceHeight=i)
+        #append the ratio to the array
+        HRArray = np.append(HRArray, ABvsHR)
 
+    return heightsArray, HRArray
 
 def main():
     x, y = motion_solver(50, 45, 0.1, 'Euler')
@@ -116,5 +140,8 @@ def main():
     plt.plot(x, y, '--', x1, y1, '+')
     plt.show()
     print(ABvsHRRats(100, 15, 45, 10, 3000, 400))
+    y,x = optimal_fence_height()
+    plt.plot(x,y)
+    plt.show()
 
 main()
